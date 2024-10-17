0163cb878bd824a6578e41d96bb50cb184924772
Soo Yeon Seong <58795942+soolotus@users.noreply.github.com>
2024-10-17 06:21:08 -0600

    getAll users and create user

--- repositories/users.js
+++ repositories/users.js
@@ -12,6 +12,34 @@ class UsersRepository {
       fs.writeFileSync(this.filename, "[]");
     }
   }
+  async getAll() {
+    // // Open the file called this.filename
+    // const contents = await fs.promises.readFile(this.filename, {encoding: 'utf8'})
+   
+    // // Parse the contents
+    // const data = JSON.parse(contents)
+    // // Return the parsed data
+    // return data
+return JSON.parse(await fs.promises.readFile(this.filename, {encoding: 'utf8'}))
+
+  }
+
+  async create(attrs){
+    // {email: daea, password: dfas}
+    const records =await this.getAll()
+    records.push(attrs)
+
+    await fs.promises.writeFile(this.filename, JSON.stringify(records))
+
+  }
+}
+const test = async ()=>{
+   const repo =new UsersRepository("users.json");
+   
+   await repo.create({email:"test", password: "test"})
+
+   const users = await repo.getAll()
+    console.log(users)
 }
 
-new UsersRepository("users.json");
+test()
