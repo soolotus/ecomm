07af12eb8a740083e36b03d7cf58237fdc91e33b
Soo Yeon Seong <58795942+soolotus@users.noreply.github.com>
2024-10-19 23:46:28 -0600

    create, getOne

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto")
 
 class UsersRepository {
   constructor(filename) {
@@ -26,20 +27,37 @@ return JSON.parse(await fs.promises.readFile(this.filename, {encoding: 'utf8'}))
 
   async create(attrs){
     // {email: daea, password: dfas}
+    attrs.id = this.randomId()
     const records =await this.getAll()
     records.push(attrs)
 
-    await fs.promises.writeFile(this.filename, JSON.stringify(records))
+    await this.writeAll(records)
 
   }
+
+  async writeAll(records){
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null,2))
+  }
+
+  randomId(){
+    return  crypto.randomBytes(4).toString('hex')
+
+  }
+  async getOne(id){
+    const records = await this.getAll()
+    return records.find(record=>record.id === id)
+
+  }
+
 }
 const test = async ()=>{
    const repo =new UsersRepository("users.json");
-   
-   await repo.create({email:"test", password: "test"})
 
-   const users = await repo.getAll()
-    console.log(users)
+   const user = await repo.getOne("cd94eddb")
+   console.log(user)
+   
+   
+   
 }
 
 test()
