f33eddf7d9444d2b176d36bb7e9926642117b6ea
SooYeon Seong <soolotus632@gmail.com>
2024-10-23 20:52:49 -0600

    fetching a session

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["sfjklajhil"] }));
 
 app.get("/", (req, res) => {
   res.send(`
   <div>
+  Your ID is:${req.session.userId}
   <form method="POST">
   <input name="email" placeholder="email"/>
   <input name="password" placeholder="password"/>
@@ -48,7 +51,9 @@ app.post("/", async (req, res) => {
   }
   // Create a user in our user repo to represent this person
   const user = await usersRepo.create({ email, password });
+
   // Store the id of that user inside the users cookie
+  req.session.userId = user.id;
 
   res.send("Account created");
 });
