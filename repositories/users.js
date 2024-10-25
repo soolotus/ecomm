56062ce598a0bb51f605d078a7413d84a500c816
SooYeon Seong <soolotus632@gmail.com>
2024-10-25 02:45:07 -0600

    comparing passwords

--- repositories/users.js
+++ repositories/users.js
@@ -47,6 +47,19 @@ class UsersRepository {
     return record;
   }
 
+  async comparePasswords(saved, supplied) {
+    // saved -> password saved in our database. 'hashed.salt'
+    // supplied -> password given to us by a user trying sign in
+    //
+    // const result = saved.split(".");
+    // const hashed = result[0];
+    // const salt = result[1];
+    const [hashed, salt] = saved.split(".");
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuf.toString("hex");
+  }
+
   async writeAll(records) {
     await fs.promises.writeFile(
       this.filename,
