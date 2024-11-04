9ec4f3bbaa23a9fa7ef9f4e1da39c9e4523795a4
SooYeon Seong <soolotus632@gmail.com>
2024-11-04 14:25:23 -0700

    saving the image

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cookieSession({ keys: ["sfjklajhil"] }));
 app.use(authRouter);
 app.use(productsRouter);
