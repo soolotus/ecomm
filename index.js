c89a559980d22af5c4a611434707220162640515
SooYeon Seong <soolotus632@gmail.com>
2024-11-06 12:51:40 -0700

    Shopping cart boilerplate

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
@@ -11,9 +12,11 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieSession({ keys: ["sfjklajhil"] }));
+
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening...");
