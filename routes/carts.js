fecb1c89085b873b6e480f3ae14df58a8303f882
SooYeon Seong <soolotus632@gmail.com>
2024-11-06 14:31:21 -0700

    Adding items to a cart

--- routes/carts.js
+++ routes/carts.js
@@ -16,11 +16,19 @@ router.post("/cart/products", async (req, res) => {
     // We have a cart! Let's get it from the repository
     cart = await cartsRepo.getOne(req.session.cartId);
   }
-  console.log(cart);
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
 
   // Either increment quantity for existing product
-
+  if (existingItem) {
+    existingItem.quantity++;
+  }
   //   Or add new product to items array
+  else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, { items: cart.items });
 
   res.send("Product added to cart");
 });
